8dc1870ca4f25158f939918ee7cba68edbcc5e2f
daneshhem <dtryanxpress@yahoo.com>
2020-11-12 17:19:43 -0600

    message

--- dist/spadanesh.e31bb0bc.js
+++ dist/spadanesh.e31bb0bc.js
@@ -19698,7 +19698,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65039" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50581" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
